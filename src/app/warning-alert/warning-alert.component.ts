baedc1ea1b9c8e8c053a4f9b684fed2ce736e5ca
Kristopher Williams <kristopher.williams2@gmail.com>
2020-04-08 20:05:44 -0500

    first commit

---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warningAlert',
+  templateUrl: './warning-alert.component.html',
+  styles: [
+    `
+      p {
+        color: red;
+      }
+    `,
+  ],
+})
+export class WarningAlertComponent {}
